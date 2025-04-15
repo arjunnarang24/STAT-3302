2b92c6d0c324c9cc58b76c81d5cbd8cf9ab2ef39
arjunnarang24 <arjunsnarang@gmail.com>
2025-04-15 14:35:16 -0400

    Update 0_data_cleaning.R
    Season variable and working directory

--- template_for_project/template_for_project/scripts/0_data_cleaning.R
+++ template_for_project/template_for_project/scripts/0_data_cleaning.R
@@ -1,7 +1,7 @@
 ############################
 ## This script performs data cleaning
 ## Last modified: 2025-03-31
-## SP: I removed some variables
+## SP: Added season variable and changed working directory
 ############################
 ## libraries we are using 
 library(tidyverse)
@@ -53,10 +53,18 @@ longbeach <- longbeach %>%
     sex = as.factor(sex),
     intake_type = as.factor(intake_type)
   )
+
+# Make seasons
+longbeach <- longbeach %>%
+  mutate(season = case_when(
+    format(outcome_date, "%m") %in% c("12", "01", "02") ~ "Winter",
+    format(outcome_date, "%m") %in% c("03", "04", "05") ~ "Spring",
+    format(outcome_date, "%m") %in% c("06", "07", "08") ~ "Summer",
+    format(outcome_date, "%m") %in% c("09", "10", "11") ~ "Fall",
+    TRUE ~ NA_character_
+  ))
 longbeach_clean <- na.omit(longbeach %>%
-                             select(adopted, animal_type, intake_condition, sex, intake_type,outcome_date,primary_color))
-
-
+                             select(adopted, animal_type, intake_condition, sex, intake_type, outcome_date, primary_color, season))
 
 ## .rds format
 saveRDS(longbeach_clean, "../data/longbeach_clean.rds")
