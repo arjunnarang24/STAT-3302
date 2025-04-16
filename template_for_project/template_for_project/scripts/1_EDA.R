a0d547f55c7fb7e5f6057d8056fc99f0416ad538
lexiewheeler <167574314+lexiewheeler@users.noreply.github.com>
2025-04-16 16:45:37 -0400

    EDA.R

--- template_for_project/template_for_project/scripts/1_EDA.R
+++ template_for_project/template_for_project/scripts/1_EDA.R
@@ -1,6 +1,7 @@
 ## This script looks at exploratory data analysis 
-## Last modified: 2025-04-15
+## Last modified: 2025-04-16
 ## Moved the season variable to data cleaning and changed it so that the working directory is in the scripts (feel free to alter this if not satisfactory just let group know) - AN
+## added our raw adoption percentage and number of animals adopted, plotted intake condition, intake type, sex, and color as we said in our proposal. - LW
 ############################
 ## libraries we are using 
 library(ggplot2)
@@ -26,6 +27,12 @@ dim(longbeach)
 # Check for N/A 
 colSums(is.na(longbeach))
 
+# Number of animals adopted
+table(longbeach$adopted)
+
+# % of animals adopted 
+numberAdopted <- 6290/(23310+6290) *100
+
 # Check Adoption Rates
 adoption_rates <- longbeach_clean %>%
   group_by(animal_type) %>%
@@ -34,6 +41,17 @@ adoption_rates <- longbeach_clean %>%
 
 print(adoption_rates)
 
+# Number of animals per animal type
+animal_counts <- longbeach %>%
+  count(animal_type)
+
+# Plotting raw numbers to just see how many of each animal we have
+animals <- ggplot(animal_counts, aes(x = reorder(animal_type, -n), y = n)) +
+  geom_col(fill = "steelblue") +
+  labs(title = "Number of Animals per Animal Type",
+       x = "Animal Type",
+       y = "Number of Animals")
+ggsave(plot = animals, filename = "../figures/NumberofEachAnimal.pdf")
 
 # Adoption Rates by Animal Type
 rateByType <- ggplot(adoption_rates, aes(x = reorder(animal_type, -adoption_rate), y = adoption_rate)) +
@@ -44,7 +62,6 @@ rateByType <- ggplot(adoption_rates, aes(x = reorder(animal_type, -adoption_rate
   theme_minimal()
 
 ggsave(plot = rateByType, filename = "../figures/AdoptionRatesByAnimal.pdf")
-
 
 
 # Plot proportions
@@ -82,4 +99,80 @@ seasonalPlot <- ggplot(seasonal_adoption, aes(x = season, y = adoption_rate, fil
 
 ggsave(plot = seasonalPlot, filename = "../figures/adoptionsBySeason.pdf")
 
+# Adoption rate by intake condition
+intakeCondition <- longbeach %>%
+  filter(!is.na(intake_condition) & !is.na(adopted)) %>%
+  group_by(intake_condition) %>%
+  summarise(adoption_rate = mean(adopted) * 100) %>%
+  arrange(desc(adoption_rate))
 
+# Plot adoption rate by intake condition
+rateByCondition <- ggplot(intakeCondition, aes(x = reorder(intake_condition, -adoption_rate), y = adoption_rate)) +
+  geom_col(fill = "steelblue") +
+  labs(title = "Adoption Rates by Intake Condition",
+       x = "Intake Condition",
+       y = "Adoption Rate (%)") +
+  theme_minimal() + coord_flip()
+
+ggsave(plot = rateByCondition, filename = "../figures/adoptionByCondition.pdf")
+
+# Adoption rate by Sex
+rateSex <- longbeach %>%
+  filter(!is.na(sex) & !is.na(adopted)) %>%
+  group_by(sex) %>%
+  summarise(adoption_rate = mean(adopted) * 100) %>%
+  arrange(desc(adoption_rate))
+
+# Plot adoption rate by Sex
+rateBySex <- ggplot(rateSex, aes(x = reorder(sex, -adoption_rate), y = adoption_rate)) +
+  geom_col(fill = "steelblue") +
+  labs(title = "Adoption Rates by Sex",
+       x = "Sex",
+       y = "Adoption Rate (%)") +
+  theme_minimal()
+
+ggsave(plot = rateBySex, filename = "../figures/adoptionBySex.pdf")
+
+
+# Adoption rate by intake type - different than intake condition
+# There are 2 different types of returns so I combined them here using mutate
+intakeType <- longbeach %>%
+  filter(!is.na(intake_type) & !is.na(adopted)) %>%
+  mutate(intake_type = case_when(
+    intake_type %in% c("return", "adopted animal return") ~ "return",
+    TRUE ~ intake_type)) %>%
+  group_by(intake_type) %>%
+  summarise(adoption_rate = mean(adopted) * 100) %>%
+  arrange(desc(adoption_rate))
+
+# Plot adoption rate by intake type
+rateByType <- ggplot(intakeType, aes(x = reorder(intake_type, -adoption_rate), y = adoption_rate)) +
+  geom_col(fill = "steelblue") +
+  labs(title = "Adoption Rates by Intake Type",
+       x = "Intake Type",
+       y = "Adoption Rate (%)") +
+  theme_minimal() + coord_flip() 
+
+ggsave(plot = rateByType, filename = "../figures/adoptionByType.pdf")
+
+
+# Adoption rate by Primary Color
+rateColor <- longbeach %>%
+  filter(!is.na(primary_color) & !is.na(adopted)) %>%
+  group_by(primary_color) %>%
+  summarise(adoption_rate = mean(adopted) * 100) %>%
+  arrange(desc(adoption_rate))
+
+# Top 20 adoption rate colors. There are too many to graph all on one chart
+topColors <- rateColor %>%
+  slice_max(adoption_rate, n = 20)
+
+# Plot Adoption Rate by colors using only the top 20 with the highest adoption
+rateByColor <- ggplot(topColors, aes(x = reorder(primary_color, -adoption_rate), y = adoption_rate)) +
+  geom_col(fill = "steelblue") +
+  labs(title = "Adoption Rates by Primary Color",
+       x = "Primary Color",
+       y = "Adoption Rate (%)") +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggsave(plot = rateByColor, filename = "../figures/adoptionByColor.pdf")
